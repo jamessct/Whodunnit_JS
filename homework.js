452091fb31293f8397c19d5e949ef801b793397c
James Scott <jamessct@hotmail.co.uk>
2016-09-19 21:06:12 +0100

    here ya go

---
+++ homework.js
@@ -0,0 +1,23 @@
+// Episode 1: My name is Keith .... introduces the character Kieth
+
+// Episode 2: 3 ... 5 down to 3
+
+// Episode 3: 0: Ducks, 1: Dogs, 2: Lions ...loads of animals
+
+// Episode 4: Suspects include: Jay, Val, Harvey, Rick.  Suspect three is: Kieth ... Harvey turned into Keith
+
+// Episode 5: Poirot
+
+// Episode 6: The murderer is rick
+
+//Episode 7
+
+var murderWeapon = "";
+
+switch(murderWeapon) {
+  case "false":
+    console.log("dagger")
+    break;
+  default:
+    console.log("candlestick");
+}
